e5717e2a77ba76254bd987968c0db3a0b077a473
GorCat <745318341@qq.com>
2020-10-13 10:37:09 +0800

    dev: 增加了空白工程

---
+++ FoxReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FoxReader
+//
+//  Created by GorCat on 2020/10/13.
+//  Copyright © 2020 GorCat. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FoxReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FoxReader
+//
+//  Created by GorCat on 2020/10/13.
+//  Copyright © 2020 GorCat. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
