1887a1d310a604221a1720c73741162a229d12b4
GorCat <745318341@qq.com>
2020-10-13 10:53:22 +0800

    dev: 增加了部分常用库

---
+++ CatReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatReader
+//
+//  Created by GorCat on 2020/10/13.
+//  Copyright © 2020 GorCat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '11.0'
+
+target 'CatReader' do
+  use_frameworks!
+  inhibit_all_warnings!
+
+  pod 'SnapKit'                    # 约束
+  pod 'YYKit'                      # UI 组件
+  pod 'PKHUD'                      # 指示
+
+  # Pods for CatReader
+
+end
