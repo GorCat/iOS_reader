d23772d8fae4c51b97cad95f23b51f7418e35578
GorCat <745318341@qq.com>
2020-10-13 11:27:17 +0800

    dev: 增加了基础类

---
+++ CatReader/base/BaseNavigationController.swift
@@ -0,0 +1,31 @@
+
+//
+//  BaseNavigationController.swift
+//  CatReader
+//
+//  Created by GorCat on 2020/10/13.
+//  Copyright © 2020 GorCat. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
