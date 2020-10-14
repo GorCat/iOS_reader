d2522d0a57bd902020c06455e038def5c230aa59
GorCat <745318341@qq.com>
2020-10-14 15:42:23 +0800

    dev: 增加了按钮切图和主体切图，增加了主体相关 color set

---
+++ CatReader/mian/RDReader/model/RDReadConfigManager.swift
@@ -0,0 +1,13 @@
+//
+//  RDReadConfigManager.swift
+//  CatReader
+//
+//  Created by GorCat on 2020/10/14.
+//  Copyright © 2020 GorCat. All rights reserved.
+//
+
+import UIKit
+
+class RDReadConfigManager: NSObject {
+
+}
