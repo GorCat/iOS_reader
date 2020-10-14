ca9b3b3504869f010a2508762f9ffd774007a8bb
GorCat <745318341@qq.com>
2020-10-14 17:52:58 +0800

    dev: 增加了 RDModelAgent

---
+++ CatReader/mian/RDReader/model/RDModelAgent.swift
@@ -0,0 +1,16 @@
+//
+//  RDModelAgent.swift
+//  CatReader
+//
+//  Created by GorCat on 2020/10/14.
+//  Copyright © 2020 GorCat. All rights reserved.
+//
+
+import UIKit
+
+class RDModelAgent: NSObject {
+    static let share = RDModelAgent()
+    
+    private override init() {
+    }
+}
